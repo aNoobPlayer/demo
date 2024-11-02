cd5474e36f863b2dd88689f0118b2b99b347e5ff
Master <traicute459@gmail.com>
2024-11-02 11:39:25 +0700

    File moi

--- Source.cpp
+++ Source.cpp
@@ -3,20 +3,30 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
+<<<<<<< HEAD
 {
 	return a - b;
 }
 int tich(int a, int b);
+=======
+int tich(int a, int b); {
+	return a * b;
+}
+>>>>>>> d294b53 (Tich file)
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	int a,b;
-	cout <<"Nhap vao a";
+	cout <<"Nhap vao a,b";
 	cin>>a;
-	cout<<"Nhap vao b";
 	cin>>b;
+<<<<<<< HEAD
 	cout<<"Hieu 2 ab la "<<hieu(a,b);
+=======
+
+	cout<<"Tich 2 ab la "<<tich(a,b);
+>>>>>>> d294b53 (Tich file)
 	system("pause");
 	return 0;
 }
