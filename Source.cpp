0e3c6233172388d974081dbf2d4b92de447fd9c6
Master <traicute459@gmail.com>
2024-11-02 11:07:01 +0700

    Tong file

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 int tong(int a, int b);
+{
+return a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a,b;
+	cout <<"Nhap vao a";
+	cin>>a;
+	cout<<"Nhap vao b";
+	cin>>b;
+	cout<<"Tong 2 ab la "<<tong(a,b);
 	system("pause");
 	return 0;
 }
