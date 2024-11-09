d948c3a0779c7b98c9595217d24f5439825db374
Tu <tuvo5556@gmail.com>
2024-11-09 11:11:59 +0700

    i love...

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int hieu(int a, int b);
+int n;
+bool Ktsnt(int n){
+	for(int i=2;i<sqrt(n);i++){
+		if(n%i==0)
 
+		return true;
+		return false;	}
+}
 	
 int tich(int a, int b); {
 	return a * b;
