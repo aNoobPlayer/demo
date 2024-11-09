a6d595c13335566d653e67c10d9f505a61298ee9
Tu <tuvo5556@gmail.com>
2024-11-09 10:34:07 +0700

    i love tam

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+
 int hieu(int a, int b);
-{
-	return a - b;
-}
+
+	
 int tich(int a, int b); {
 	return a * b;
 }
 float thuong(int a, int b);
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	int a,b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIThub";
+
 	cout <<"Nhap vao a,b";
-	cin>>a;
-	cin>>b;
-	cout<<"Hieu 2 ab la "<<hieu(a,b);
-	cout<<"Tich 2 ab la "<<tich(a,b);
-	system("pause");
-	return 0;
 }
